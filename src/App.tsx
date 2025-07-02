681f4b1841aa44e25d525b98fd03cf3a41db8e97
박종범 <jbpark03@gmail.com>
2025-07-02 15:37:53 +0900

    use interface to define complex props

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-function MyButton({ label, disabled }: { label: string, disabled: boolean }) {
+interface ButtonProp {
+  label: string;
+  disabled: boolean;
+}
+
+function cancelOrder() {
+  alert("주문 취소");
+}
+function MyButton({ label, disabled }: ButtonProp) {
   return (
-    <button disabled={disabled}>{label}</button>
+    <button disabled={disabled} onClick={cancelOrder}>{label}</button>
   )
 }
 
 export default function BumSoap() {
   return (
     <div>
-      <h1>범이비누 가게</h1>
-      <MyButton label="구매 취소" disabled={true} />
+      <h1>범이비누 주문</h1>
+      <MyButton label="구매 취소" disabled={false} />
     </div>
   )
 }
